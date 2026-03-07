7ce25db8dd8b41ce817afa99c179498fab76b15c
onchezz <brianonchezz@gmail.com>
2026-03-07 11:56:15 +0300

    stable docs

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -77,6 +77,7 @@ const config = {
         logo: {
           alt: 'Hypertill DB Logo',
           src: 'img/logo.svg',
+          srcDark: 'img/logo-dark.svg',
         },
         items: [
           {
